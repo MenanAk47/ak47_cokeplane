4aeb706937ddf197deedb0a3491bf0ce0525f28d
MenanAk47 <35970476+MenanAk47@users.noreply.github.com>
2021-11-05 18:28:06 +0600

    update sql

--- sql/item-limit.sql
+++ sql/item-limit.sql
@@ -1,4 +1,7 @@
+DELETE FROM items WHERE name = 'cokebrick';
+DELETE FROM items WHERE name = 'coke_pooch';
+
 INSERT INTO `items` (name, label, limit) VALUES
 	('cokebrick', 'Brick of coke', -1),
-	('coke_pooch', 'Bag of coke', -1,)
+	('coke_pooch', 'Bag of coke', -1)
 ;
